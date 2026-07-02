da03f4fe74fc08d74ac67e91ab7b3c9eb47d9901
YSL2683 <dbstjd55@seoultech.ac.kr>
2026-07-02 20:47:21 +0900

    Update training scripts for robosuite tasks with adjusted training steps and demo densities

--- run_decouple_lift.sh
+++ run_decouple_lift.sh
@@ -9,7 +9,7 @@ COMMON_ARGS="--domain_name robosuite_lift \
   --observation_type pixel --encoder_type pixel \
   --pre_transform_image_size 128 --image_size 112 --agent dino_e2c_sac \
   --critic_lr 0.001 --actor_lr 0.001 --eval_freq 2000 --batch_size 128 \
-  --num_train_steps 100000 --save_tb --save_video --replay_buffer_load_dir ./demo/robosuite_lift/10 \
+  --num_train_steps 50000 --save_tb --save_video --replay_buffer_load_dir ./demo/robosuite_lift/5 \
   --replay_buffer_keep_loaded --init_steps 0 --save_sac --conv_layer_norm \
   --v_clip_low -100 --v_clip_high 100 --seed 1 --num_eval_episodes 20 --encoder_feature_dim 32 \
   --final_demo_density 0.15"
--- run_decouple_pick_place_can.sh
+++ run_decouple_pick_place_can.sh
@@ -9,10 +9,10 @@ COMMON_ARGS="--domain_name robosuite_pick_place_can \
   --observation_type pixel --encoder_type pixel \
   --pre_transform_image_size 128 --image_size 112 --agent dino_e2c_sac \
   --critic_lr 0.001 --actor_lr 0.001 --eval_freq 2000 --batch_size 128 \
-  --num_train_steps 100000 --save_tb --save_video --replay_buffer_load_dir ./demo/robosuite_pick_place_can/10 \
+  --num_train_steps 250000 --save_tb --save_video --replay_buffer_load_dir ./demo/robosuite_pick_place_can/20 \
   --replay_buffer_keep_loaded --init_steps 0 --save_sac --conv_layer_norm \
   --v_clip_low -100 --v_clip_high 100 --seed 1 --num_eval_episodes 20 --encoder_feature_dim 32 \
-  --final_demo_density 0.15"
+  --final_demo_density 0.2"
 
 echo "=== 제안 방법론(decouple) 실험 시작 (pick_place_can) ==="
 python train.py $COMMON_ARGS --work_dir ./data/robosuite_pick_place_can/lane_dino_decouple --reward_camera decouple
